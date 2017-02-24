c7be9c4ca7ca24cafe3c78e751490002ce495dab
mikima25 <vtalism20@gmail.com>
2017-02-24 18:56:59 +0300

    js modification

--- js/scripts.js
+++ js/scripts.js
@@ -1,26 +1,27 @@
+//back end
+function conversion(number) {
+    var entry = [];
+    for (var i = 1; i <=number; i++) {
+        if (i % 15 === 0) {
+            entry.push("pingpong");
+        } else if (i % 3 === 0) {
+            entry.push("ping");
+        } else if (i % 5 === 0) {
+            entry.push("pong")
+        } else {
+            entry.push(i)
+        }
+    }
+    return entry;
+}
 
-var entry = []
-var myNumber = function(number) {
-    for(var i= 0; i <= number; i ++) {
-if (i % 15 === 0) {
-    entry.push("pingpong"); {
-}else if (i % 5 === 0) {
-    entry.push("pong");
-}else if (i % 3 === 0) {
-    entry.push("ping");
-}else
-    entry.push(i);
-}
-return entry;
-}
-}
-
+//front end
 $(document).ready(function() {
-    $("#form-group").submit(function() {
-        var number = parseInt($("input#number").val());
-        var result = myNumber(number);
+    $("#blank form").submit(function() {
+        var inputuser= parseInt($("input#number").val());
+        var result = conversion(inputuser);
         result.forEach((function)(output) {
-        $("#output").append("<li>" + entry + "</li>");
+        $("#output").append("<li>" + output + "</li>");
         event.preventDefault();
     });
     });
