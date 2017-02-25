325a1a7454d06258b4f46b2e48ed72ea2299bdfb
mikima25 <vtalism20@gmail.com>
2017-02-25 19:14:13 +0300

    back-end logic added

--- js/scripts.js
+++ js/scripts.js
@@ -1,28 +1,15 @@
-//back end
-function conversion(number) {
-    var entry = [];
-    for (var i = 1; i <=number; i++) {
-        if (i % 15 === 0) {
-            entry.push("pingpong");
-        } else if (i % 3 === 0) {
-            entry.push("ping");
-        } else if (i % 5 === 0) {
-            entry.push("pong")
-        } else {
-            entry.push(i)
-        }
-    }
-    return entry;
+//back-end
+function myPingPong(userInput) {
+var entry = [];
+for (var i = 1; i <= userInput; i++) {
+ if ((i % 15) === 0) {
+   entry.push ("Ping-Pong");
+ } else if ((i % 5) === 0) {
+   entry.push ("Pong");
+ } else if ((i % 3) === 0) {
+   entry.push ("Ping");
+ } else
+   entry.push (i);
 }
-
-//front end
-$(document).ready(function() {
-    $("#blank form").submit(function() {
-        var inputuser= parseInt($("input#number").val());
-        var result = conversion(inputuser);
-        result.forEach((function)(output) {
-        $("#output").append("<li>" + output + "</li>");
-        event.preventDefault();
-    });
-    });
-});
+return entry;
+}
